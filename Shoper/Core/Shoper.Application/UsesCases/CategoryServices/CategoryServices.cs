2f8bab9e20b65df41b3fd07e03787fbd3d8a2b52
Efe Hancıoğlu <efehancioglu0@gmail.com>
2026-06-13 12:28:37 +0300

    feat: added category services

---
+++ Shoper/Core/Shoper.Application/UsesCases/CategoryServices/CategoryServices.cs
@@ -0,0 +1,46 @@
+﻿using Shoper.Application.Dtos.CategoryDtos;
+using Shoper.Application.Interfaces;
+using Shoper.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoper.Application.UsesCases.CategoryServices
+{
+    public class CategoryServices : ICategoryServices
+    {
+        private readonly IRepository<Category> _repository;
+
+        public CategoryServices(IRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+
+        public Task CreateCategoryAsync(CreateCategoryDto model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultCategoryDto> GetByIdCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(UpdateCategoryDto model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Shoper/Core/Shoper.Application/UsesCases/CategoryServices/ICategoryServices.cs
@@ -0,0 +1,18 @@
+﻿using Shoper.Application.Dtos.CategoryDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoper.Application.UsesCases.CategoryServices
+{
+    public interface ICategoryServices
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        Task<ResultCategoryDto> GetByIdCategoryAsync(int id);
+        Task CreateCategoryAsync(CreateCategoryDto model);
+        Task UpdateCategoryAsync(UpdateCategoryDto model);
+        Task DeleteCategoryAsync(int id);
+    }
+}
