14734f198cc0cb2f7722f5e1b1f100b3649a6abf
Efe Hancıoğlu <efehancioglu0@gmail.com>
2026-06-14 18:43:39 +0300

    feat: added AppDbContext and Repository for database

---
+++ Shoper/Infrastructure/Shoper.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shoper.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoper.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=EFE\\SQLEXPRESS; database=Shoper; IntegratedSecurity=True; TrustServerCertificate=True");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Shoper/Infrastructure/Shoper.Persistence/Repositories/Repository.cs
@@ -0,0 +1,45 @@
+﻿using Shoper.Application.Interfaces;
+using Shoper.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoper.Persistence.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task CreateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
