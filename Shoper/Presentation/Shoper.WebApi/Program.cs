94f719676639018a931c391d5d670cc03360e93e
Efe Hancıoğlu <efehancioglu0@gmail.com>
2026-06-19 16:18:54 +0300

    feat: added services to Program.cs

--- Shoper/Presentation/Shoper.WebApi/Program.cs
+++ Shoper/Presentation/Shoper.WebApi/Program.cs
@@ -1,7 +1,14 @@
+using Shoper.Application.Interfaces;
+using Shoper.Application.UsesCases.CategoryServices;
+using Shoper.Persistence.Context;
+using Shoper.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+builder.Services.AddScoped<ICategoryServices, CategoryServices>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
